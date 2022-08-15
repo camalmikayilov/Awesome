ae6c5e4f0bacab875b998191afc497a151fe62d8
camalmikayilov <camalmikayilov1@gmail.com>
2022-08-15 09:40:10 +0400

    Awesome Page

---
+++ app.js
@@ -0,0 +1,5 @@
+const menuBar = document.querySelector(".navbar_content__icon");
+const menuNav = document.querySelector(".menu_nav");
+const menuBarImg = document.querySelector(".navbar_content__icon");
+menuBar.addEventListener("click",()=>{
+    menuNav.classList.toggle();})
